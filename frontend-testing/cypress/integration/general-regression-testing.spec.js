fe80391ddbf44642916ac11bcf567908b8aa0f8f
Matas Budrikas <budrikas.matas@gmail.com>
2021-11-04 19:16:49 +0100

    part 1

--- frontend-testing/cypress/integration/general-regression-testing.spec.js
+++ frontend-testing/cypress/integration/general-regression-testing.spec.js
@@ -4,6 +4,11 @@ describe('common', function(){
     it('open main', function(){
 
         cy.visit('http://localhost:3000')
-        
+        cy.title() .should('include','Hotel')
+        cy.contains('Tester Hotel')
+        cy.get(':nth-child(1) > input') .type('tester2')
+        cy.get(':nth-child(2) > input') .type('sppm7qncqmVft5uECkwjLcLdEJGzM3Cw')
+        cy.get('.btn') .click()
+
     })
 })
