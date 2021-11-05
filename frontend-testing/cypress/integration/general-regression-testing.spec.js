89f398e640f39d9f2bf91b9caf6a1f4a12f81101
Matas Budrikas <budrikas.matas@gmail.com>
2021-11-05 18:43:48 +0100

    v.0.1.0

--- frontend-testing/cypress/integration/general-regression-testing.spec.js
+++ frontend-testing/cypress/integration/general-regression-testing.spec.js
@@ -1,7 +1,7 @@
-/// <reference types="cypress"/>
-
 describe('common', function(){
     it('open main', function(){
+
+        // Login cycle
 
         cy.visit('http://localhost:3000')
         cy.title() .should('include','Hotel')
@@ -10,8 +10,47 @@ describe('common', function(){
         cy.get(':nth-child(2) > input') .type('GteteqbQQgSr88SwNExUQv2ydb7xuf8c')
         cy.get('.btn') .click()
         cy.contains('Overview')
-        cy.get('.user > .btn') .click()
-        cy.contains('Tester Hotel')
+
+        // Rooms cycle
+
+        cy.get(':nth-child(1) > .btn') .click()
+        cy.get('a.btn:nth-child(2)') .click()
+        cy.contains('New Room')
+        cy.get('a.btn:nth-child(1)') .click()
+        cy.get('a.btn:nth-child(1)') .click()
+        cy.contains('Overview')
+
+        // Clients cycle
+
+        cy.get('div.block:nth-child(2) > a:nth-child(3)') .click()
+        cy.get('a.btn:nth-child(2)') .click()
+        cy.contains('New Client')
+        cy.get('a.btn:nth-child(1)') .click()
+        cy.get('a.btn:nth-child(1)') .click()
+        cy.contains('Overview')
+
+        // Bills cycle
+
+        cy.get('div.block:nth-child(3) > a:nth-child(4)') .click()
+        cy.get('a.btn:nth-child(2)') .click()
+        cy.contains('New Bill')
+        cy.get('a.btn:nth-child(1)') .click()
+        cy.get('a.btn:nth-child(1)') .click()
+        cy.contains('Overview')
+
+        // Reservations cycle
+
+        cy.get('div.block:nth-child(4) > a:nth-child(4)') .click()
+        cy.get('a.btn:nth-child(2)') .click()
+        cy.contains('New Reservation')
+        cy.get('a.btn:nth-child(1)') .click()
+        cy.get('a.btn:nth-child(1)') .click()
+        cy.contains('Overview')
+
+        // Exit cycle
+
+        cy.get('button.btn') .click()
+        cy.contains('Login')
 
     })
 })
